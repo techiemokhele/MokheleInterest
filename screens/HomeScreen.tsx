4adde91180acb191269576b0f072a5298e138131
techiemokhele <neomokhele23@gmail.com>
2022-06-27 20:25:28 +0200

    profile screen build

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,45 +1,6 @@
-import { StyleSheet, Image, ScrollView, FlatList } from "react-native";
-
-import { View } from "../components/Themed";
-import { RootTabScreenProps } from "../types";
-
-import Pin from "../components/Pin";
 import pins from "../assets/data/pins";
+import MasonryList from "../components/MasonryList";
 
-export default function HomeScreen({
-  navigation,
-}: RootTabScreenProps<"TabOne">) {
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        {/* first column: even index */}
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 0)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
-
-        {/* second column: odd index */}
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 1)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
-      </View>
-    </ScrollView>
-  );
+export default function HomeScreen() {
+  return <MasonryList pins={pins} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 10,
-  },
-  column: {
-    flex: 1,
-  },
-});
--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,28 +1,75 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image, ScrollView } from "react-native";
 
 import { Text, View } from "../components/Themed";
+import { AntDesign, Entypo, Feather } from "@expo/vector-icons";
+import pins from "../assets/data/pins";
+import MasonryList from "../components/MasonryList";
 
-export default function TabTwoScreen() {
+export default function ProfileScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.profileInfo}>
+        <View style={styles.icons}>
+          <Feather name="share" size={24} color="black" style={styles.icon} />
+          <Entypo
+            name="dots-three-horizontal"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+        </View>
+        <Image
+          source={{
+            uri: "https://avatars.githubusercontent.com/u/67394147?v=4",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>
+          Neo Mokhele{" "}
+          <AntDesign
+            name="checkcircle"
+            size={16}
+            color="#3b5998"
+            style={{ marginLeft: 5, marginBottom: 2 }}
+          />
+        </Text>
+        <Text style={styles.subtitle}>12K Followers | 732 Followings</Text>
+      </View>
+      <MasonryList pins={pins} />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    width: "100%",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    margin: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  subtitle: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: "#181818",
+    margin: 10,
+  },
+  image: {
+    width: 200,
+    aspectRatio: 1,
+    borderRadius: 200,
+    marginVertical: 10,
+  },
+  profileInfo: {
+    alignItems: "center",
+  },
+  icons: {
+    flexDirection: "row",
+    alignSelf: "flex-end",
+    padding: 10,
+  },
+  icon: {
+    paddingHorizontal: 10,
   },
 });
